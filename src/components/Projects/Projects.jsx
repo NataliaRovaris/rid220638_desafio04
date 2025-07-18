85fa7cabb93d6d9c49a32bc106927d7ff176d46d
NataliaRovaris <nataliarovaris@hotmail.com>
2025-07-18 17:06:59 -0300

    Feat: create title component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,16 @@
+// ASSETS
+import './Projects.css'
+
+// COMPONENTS
+import Title from '../Title/Title'
+
+function Projects () {
+
+    return (
+        <>
+            <Title title='Projetos' position='right'/>
+        </>
+    )
+}
+
+export default Projects
